d7f7da9cc473cc1e54af42cb15dfdf71504401bd
LolDragoon <Alex_Havas@hotmail.com>
2021-01-21 16:51:14 -0500

    options now have a list of who has been followed

--- options.js
+++ options.js
@@ -1,3 +1,10 @@
+const timeConversionObj = {
+  seconds: 1000,
+  minutes:60000,
+  hours:3600000,
+  days:86400000,
+}
+
 function save_options() {
   let time = document.getElementById('time').value;
   let scale = document.getElementById('scale').value;
@@ -23,11 +30,23 @@ function restore_options() {
   chrome.storage.sync.get({
     InstaFavoriteTime: 60,
     InstaFavoriteScale: "days",
-    InstaFavoriteActive: true
+    InstaFavoriteActive: true,
+    users:{}
+
   }, function(items) {
     document.getElementById('time').value = items.InstaFavoriteTime;
     document.getElementById('scale').value = items.InstaFavoriteScale;
     document.getElementById('active').checked = items.InstaFavoriteActive;
+    for (const user in items.users) {
+      //let d = new Date(items.users[user].date)
+      let timeLeft = items.users[user].amount - Math.floor((Date.now()-items.users[user].date)/timeConversionObj[items.users[user].magnitude])
+      let newLi = document.createElement("li")
+      newLi.innerHTML = `<a href= "https://instagram.com/${user}" target="_blank">${user}</a> in ${timeLeft} ${items.users[user].magnitude}`
+      //let text = document.createTextNode(`${user} in ${timeLeft} ${items.users[user].magnitude}`)
+      //newLi.appendChild(text)
+      document.getElementById("list").appendChild(newLi)
+    }
+    
   });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
